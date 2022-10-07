ca72f656b6a3eb5ca87644152971f4dab3b45d57
argituri <kmasuo@utu.fi>
2022-10-07 23:47:56 +0300

    hmm2

--- excelc.py
+++ excelc.py
@@ -1,24 +1,40 @@
 # imports
 # from sys import platform as p
-import pylightxl as xl
-import sys, os, re, csv, datetime, logging
+import csv
+import datetime
+import logging
+import os
+import re
+import sys
 from pathlib import Path
+
+import pylightxl as xl
+
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 # Check operating system ?
 
 
 # Read lines from file
 
-filename = re.sub('', str(datetime.datetime.now()))
-lines = Path('lines.txt').read_text()
-excel_file_paths = []
+filename = re.sub('\s', '_', str(datetime.datetime.now()))
+#lines = open('lines.txt', 'r').readlines()
+lines = open('lines.txt', 'r').read().splitlines()
+xlsxmatch_re = re.compile('.*\.xlsx$')
+
+#excel_file_paths = []
 
 # Add found excel file paths to variable
 for line in lines:
     logging.debug('searching .... %s', line)
     # check files in folder, add excel file paths to variable
-    files = os.listdir(line)
-    excel_file_paths.extend(re.findall('.*\.xlsx$', files))
+    try:
+        files = os.listdir(line)
+        excel_file_paths = [x for x in files if xlsxmatch_re.match(x)]
+    #    for file in files:
+    #        if re.match('.*\.xlsx$', file):
+    #            excel_file_paths.append(str(file))
+    except FileNotFoundError as e:
+        logging.debug("File not found ... %s", e)
 
 if len(excel_file_paths) < 1:
     logging.debug('No excel file paths found!')
@@ -26,20 +42,12 @@ if len(excel_file_paths) < 1:
 
 try:
     for excel_file_path in excel_file_paths:
-        logging("opening file %s", excel_file_path)
+        logging.debug("opening/writing rows from: %s", excel_file_path)
         db = xl.readxl(fn=excel_file_path)
         with open('%s.csv', filename) as f:
             writer = csv.writer(f, delimiter=';')
-            writer
             writer.writerows(db.ws(ws='Sheet1').rows)
-            #for row in db.ws(ws='Sheet1').rows:
+            # for row in db.ws(ws='Sheet1').rows:
             #    for element in row:
 except Exception as e:
-    logging.error(e);
-
-
-
-
-
-
-
+    logging.error(e)
